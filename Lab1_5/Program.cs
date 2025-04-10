57dd244d527d7e0cde856f8ac23203c50299d183
savelii-bondarenko <savabondarenko2016@gmail.com>
2025-04-10 20:49:38 +0300

    Тепер все працює

--- Lab1_5/Program.cs
+++ Lab1_5/Program.cs
@@ -1,8 +1,10 @@
+using Lab1_5.BusinessLogic.Services;
 using Lab1_5.DataAccess;
+using Lab1_5.DataAccess.Repositories;
 using Lab1_5.Models.Entity;
-using Microsoft.EntityFrameworkCore;
 using Lab1_5.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,16 +20,26 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseLazyLoadingProxies()
         .UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+builder.Services.AddControllersWithViews()
+    .AddRazorOptions(options =>
+    {
+        options.ViewLocationFormats.Add("/UI/Views/{1}/{0}.cshtml"); // Пошук у папці UI/Views/{Controller}/{Action}
+        options.ViewLocationFormats.Add("/UI/Views/Shared/{0}.cshtml"); // Пошук у папці UI/Views/Shared
+    });
 
 builder.Services.AddHostedService<ReservationCleanupService>();
 
 var app = builder.Build();
 
 app.UseSession();
+
 app.UseHttpsRedirection();
+
 app.UseRouting();
 app.UseAuthorization();
 
--- Lab1_5/obj/Debug/net9.0/Lab1_5.AssemblyInfo.cs
+++ Lab1_5/obj/Debug/net9.0/Lab1_5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce4b4bb6592d121c5fd9488ea3c8c7237094c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be98fa3850950d25d4befb587824ad253d34597b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
